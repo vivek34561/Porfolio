038d3a53113baa4e6a72d5dc32edf4f0acec5351
Vivek Kumar Gupta <vivek2.230104023@iiitbh.ac.in>
2026-01-13 00:56:11 +0530

    fix: add explicit build config for vercel

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+  },
   server: {
     port: 3000,
     open: true
